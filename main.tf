c3642224cfe3d756765c7a6bb1827044a05b0da4
kaz <qtmhy020@yahoo.co.jp>
2023-09-07 16:05:48 +0900

    new

---
+++ main.tf
@@ -0,0 +1,48 @@
+#config
+
+terraform {
+  required_version = ">=0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "tastylog-tfstate-bucket-kaz"
+    key     = "tastytlog-dev.tfstate"
+    region  = "ap-northeast-1"
+    profile = "terraform"
+  }
+
+
+}
+
+#provider
+
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
+
+
+provider "aws" {
+  alias   = "virginia"
+  profile = "terraform"
+  region  = "us-east-1"
+}
+
+#variables
+
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "domain" {
+  type = string
+}
